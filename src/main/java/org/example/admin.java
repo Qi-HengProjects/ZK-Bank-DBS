7c88c2f4965f47f5dee07fbeaa621941bdf9353b
Joaedes <jayden.tevel@gmail.com>
2026-04-01 23:40:23 +0800

    Sign Up username availability check completed

--- src/main/java/org/example/admin.java
+++ src/main/java/org/example/admin.java
@@ -5,6 +5,15 @@ import javax.swing.*;
 public class admin extends JPanel {
 
     public admin(){
+        this.setLayout(null);
+        JScrollPane adminContainer1 = new JScrollPane();
+        adminContainer1.setSize(500,500);
 
+        this.add(adminContainer1);
+
+
+        //make a white side bar that has Account Application and Loan Application Approval Pending
+        // Enum status.. pending, approve
+        //can use back the cardlayout from main
     }
 }
